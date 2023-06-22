14edf94b5d062c6a7897c041ee4f2bcf8700e58c
Bhavin Pandya <pandyab@rba.gov.au>
2023-06-22 11:16:49 +1000

    trivia based on react

--- trivia/src/trivia/triviatemplate.js
+++ trivia/src/trivia/triviatemplate.js
@@ -1,11 +1,12 @@
 import { ToastContainer } from "react-toastify"
 export default function TriviaTemplate(props) {
+    console.log(props.questionOptions.length)
     return (
         <div className="mainContainer">
             {props.questionOptions}
             <div className="d-grid gap-2 d-md-flex">
                 <button type="button" className="btn btn-outline-primary btn-sm" onClick={props.handlePrev} hidden={props.prevButtonHidden}>Prev</button>
-                <button type="button" className="btn btn-outline-primary btn-sm" onClick={props.handleSubmit}>{props.buttonValue}</button>                
+                <button type="button" className="btn btn-outline-primary btn-sm" hidden={!props.questionOptions.length} onClick={props.handleSubmit}>{props.buttonValue}</button>                
                 <button type="button" className="btn btn-outline-primary btn-sm" onClick={props.handleRestart} hidden={!props.checkedAnswer}>Restart</button>                
             </div>
             <ToastContainer />
