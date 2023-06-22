697b2351e29416e12ea73765ea4fc96c41ef472f
Bhavin Pandya <pandyab@rba.gov.au>
2023-06-22 12:04:32 +1000

    final

--- trivia/src/card/cardtemplate.js
+++ trivia/src/card/cardtemplate.js
@@ -1,8 +1,10 @@
 import Question from "../question/question";
 import Options from "../options/options";
+import "./card.css"
 export default function CardTemplate(props) {
     return (
         <div>
+            <div className="questionNumber">{props.props.item.id} / 10</div>
             <Question key={`'q' {props.props.item.id}`} item={props.props.item} id={props.props.item.id} />
             <Options key={`'a' {props.props.item.id}`} item={props.props.item} setAnswer={props.props.setAnswer} checkedAnswer={props.props.checkedAnswer} id={props.props.item.id} />
         </div>
