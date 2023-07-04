f36eae8b373f87790f04b5b30a68a37eb80c982f
Bhavin Pandya <pandyab@rba.gov.au>
2023-07-04 12:06:39 +1000

    changes

---
+++ sterlingui/src/components/candidates/candidatedetails.js
@@ -0,0 +1,12 @@
+export default function CandidateDetails() {
+    return (
+        <div className='container-fuild p-3  h-100 align-items-center justify-content-center'>
+            <div className="card shadow d-flex p-3 mb-5 bg-white rounded">
+                <h5 className="d-flex justify-content-center border border-primary rounded bg-success">Candiate List</h5>
+                <div className="card-body">
+                    Testing      
+                </div>
+            </div>
+        </div>
+    )
+}
--- sterlingui/src/components/candidates/candidates.js
+++ sterlingui/src/components/candidates/candidates.js
@@ -1,3 +1,87 @@
+import DataTable from 'react-data-table-component';
+import CandidateDetails from './candidatedetails';
+
+const columns = [
+    {
+        name: 'Title',
+        selector: row => row.title,
+        sortable: true,
+    },
+    {
+        name: 'Year',
+        selector: row => row.year,
+        sortable: true,
+    },
+];
+
+const data = [
+    {
+        id: 1,
+        title: 'Beetlejuice',
+        year: '1988',
+    },
+    {
+        id: 2,
+        title: 'Ghostbusters',
+        year: '1984',
+    },
+    {
+        id: 3,
+        title: 'Ghostbusters',
+        year: '1984',
+    },
+    {
+        id: 4,
+        title: 'Ghostbusters',
+        year: '1984',
+    },
+    {
+        id: 5,
+        title: 'Ghostbusters',
+        year: '1984',
+    },
+    {
+        id: 6,
+        title: 'Ghostbusters',
+        year: '1984',
+    },
+    {
+        id: 7,
+        title: 'Ghostbusters',
+        year: '1984',
+    },
+    {
+        id: 8,
+        title: 'Ghostbusters',
+        year: '1984',
+    },
+    {
+        id: 9,
+        title: 'Ghostbusters',
+        year: '1984',
+    },
+
+]
+
+const tableCustomStyles = {
+    headRow: {
+      style: {
+        color:'#223336',
+        backgroundColor: '#e7eef0'
+      },
+    },
+    rows: {
+      style: {
+        color: "green",
+        backgroundColor: "STRIPEDCOLOR"
+      },
+      stripedStyle: {
+        color: "red",
+        backgroundColor: "#dfe7f5"
+      }
+    }
+  }
+
 export default function Candidates() {
 
     return (
@@ -5,36 +89,20 @@ export default function Candidates() {
             <div className="card shadow d-flex p-3 mb-5 bg-white rounded">
                 <h5 className="d-flex justify-content-center border border-primary rounded bg-success">Candiate List</h5>
                 <div className="card-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <DataTable                        
+                        columns={columns}
+                        data={data}
+                        pagination
+                        selectableRows
+                        dense
+                        highlightOnHover
+                        expandableRows
+                        expandableRowsComponent={CandidateDetails}                        
+                        fixedHeader
+                        fixedHeaderScrollHeight="300px"
+                        striped                        
+                        customStyles={tableCustomStyles}                       
+                    />
                 </div></div></div>
     )
 }
